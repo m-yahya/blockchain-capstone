26e026a3fbcec2b08f78402c7b180737c193c3a8
m-yahya <yahya_gis@hotmail.com>
2019-12-11 10:04:22 +0100

    Update migrations

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 // migrating the appropriate contracts
+var RealEstateERC721Token = artifacts.require("./RealEstateERC721Token.sol");
 var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+module.exports = function (deployer) {
+  deployer.deploy(RealEstateERC721Token);
+  deployer.deploy(SquareVerifier).then(() => {
+    return deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
+  });
 };
