65e18243022475d37f5c95f34cc0b4635ea9347c
m-yahya <yahya_gis@hotmail.com>
2019-12-12 15:08:35 +0100

    Add tests for SolnSquareVerifier contract

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,47 @@
+let SquareVerifier = artifacts.require('SquareVerifier');
+let SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+let proof = require('../../zokrates/code/square/proof');
+
 // Test if a new solution can be added for contract - SolnSquareVerifier
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+contract('TestSolnSquareVerifier', accounts => {
+    const account_one = accounts[0]
+    const account_two = accounts[1]
+
+    beforeEach(async function () {
+        const squareVerifier = await SquareVerifier.new({ from: account_one })
+        this.contract = await SolnSquareVerifier.new(squareVerifier.address, {
+            from: account_one
+        })
+    })
+
+    it('Test if a new solution can be added for contract - SolnSquareVerifier', async function () {
+        let result = await this.contract.mintToken(account_two, 1,
+            proof.proof.a,
+            proof.proof.b,
+            proof.proof.c,
+            proof.inputs,
+            { from: account_one }
+        )
+
+        assert.equal(result.logs[0].event, 'SolutionAdded', 'Failed to add a solution')
+    })
+    // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+
+    it('Test if an ERC721 token can be minted for contract - SolnSquareVerifier', async function () {
+        let mint = true
+        try {
+            await this.contract.mintToken(account_two, 1,
+                proof.proof.a,
+                proof.proof.b,
+                proof.proof.c,
+                proof.inputs,
+                { from: account_one }
+            )
+        } catch (e) {
+            mint = false
+        }
+
+        assert.equal(mint, true, "cannot mint  a token");
+    })
+})
